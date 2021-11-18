3acc2a8b7e413a9e41fde60d9af79d55085bcd5d
sidwebworks <xodedeveloper@gmail.com>
2021-11-18 21:27:13 +0530

    🎉 Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Editor } from "../components/playground/Editor";
+
+const Home: NextPage = () => {
+	return (
+		<main>
+			<Editor />
+		</main>
+	);
+};
+
+export default Home;
