88c44b881436da3204ffc9c52e06a4bae1e80ae4
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-06-06 01:52:09 +0530

    fix: build error

---
+++ src/components/preview/preview.tsx
@@ -0,0 +1,47 @@
+import {
+  ReactEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
+import { useTypedSelector } from "src/utils/store/store";
+import { createSnippet } from "./preview.helpers";
+
+enum MessageType {
+  LOAD = 0,
+  UPDATE = 1,
+}
+
+const Preview: React.FC = () => {
+  const { css, html, js } = useTypedSelector((p) => p.preview.source);
+  const iframe = useRef<HTMLIFrameElement>(null);
+
+  const result = useMemo(
+    () => createSnippet({ html, css, js }),
+    [html, css, js]
+  );
+
+  useEffect(() => {
+    if (!iframe.current?.contentWindow) return;
+    iframe.current.srcdoc = result;
+  }, [result]);
+
+  const onLoad: ReactEventHandler<HTMLIFrameElement> = useCallback((ev) => {},
+  []);
+
+  return (
+    <div className={"preview-wrapper h-full"}>
+      <iframe
+        ref={iframe}
+        onLoad={onLoad}
+        title="preview"
+        srcDoc={result}
+        className="h-full"
+        sandbox="allow-scripts allow-same-origin allow-modals"
+      />
+    </div>
+  );
+};
+
+export default Preview;
